bb99c217fe98e4343eeb4f9353dc5091ebc1fca5
Gabriel Pelosi <1180017@isep.ipp.pt>
2021-08-16 15:48:31 +0100

    navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Navbar.scss'
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="nav-logo">
+                    <img src="https://cdn0.iconfinder.com/data/icons/shopping-icons-rounded/110/Online-Shopping-512.png" alt="" />
+                </div>
+                <div className="nav-items">
+                    <ul>
+                        <li>Home</li>
+                        <li>Products</li>
+                        <li>Historical</li>
+                    </ul>
+                </div>
+                <div className="cart-container">
+                    <img src="https://image.flaticon.com/icons/png/512/263/263142.png" className="cart-btn" />
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
