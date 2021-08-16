5b546e201a3fa04a0175d7de2bb0520558737448
Gabriel Pelosi <1180017@isep.ipp.pt>
2021-08-16 18:53:26 +0100

    footer changes styles

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.scss'
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="footer">
+                <div className="container">
+                    fsdlkfsdlk
+                </div>
+                <div className="container-items">
+                    fnsdlfnsd
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
