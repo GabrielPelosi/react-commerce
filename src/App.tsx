1e51c5240cdbadc9854cad98f3878b709d325a6f
Gabriel Pelosi <1180017@isep.ipp.pt>
2021-08-17 15:55:55 +0100

    state to cart items animation and rrot files config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import './App.scss';
+import {useState} from 'react'
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import ProductList from './components/ProductList/ProductList';
+import Cart from './components/Cart/Cart';
 
 function App() {
+
+  const [cartMenuState, setCartMenuState] = useState<Boolean>(false)
+  
   return (
     <>
-      <Navbar />
-      <Header/>
+      
+      <Navbar cartMenuState={cartMenuState} setCartMenuState={setCartMenuState} />
+      <Cart cartMenuState={cartMenuState}/>
+      <Header />
       <ProductList />
-      <Footer/>
+      <Footer />
     </>
   );
 }
